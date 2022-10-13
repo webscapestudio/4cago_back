383f4cf6093dcde3286e4c607a9a018706b67214
Dianovexitgit config --global user.email you@example.com <you@example.com>
2022-10-13 15:20:55 +0300

    Никита проверь мою работу

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,7 @@
     <body class="antialiased">
         <p>Новая ветка</p>
         <p>Теперь у нас новая ветка для разработки</p>
+        <p>Теперь у нас новая ветка для разработки (Отпочковался)</p>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed m-auto top-0 right-0 px-6 py-4 sm:block">
