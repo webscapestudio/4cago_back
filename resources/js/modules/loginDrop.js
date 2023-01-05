7e7f9eccdf249397521e9710c8ce931d30e792af
Евгений Дианов <dianov.js@gmail.com>
2023-01-05 16:17:24 +0300

    fix drop menu

--- resources/js/modules/loginDrop.js
+++ resources/js/modules/loginDrop.js
@@ -1,12 +1,18 @@
 const openDrop = document.querySelector(".login__block");
 const openItem = document.querySelector(".create");
-const menu = document.querySelector(".profile__item");
+const menu = document.querySelector(".profile__menu");
 
 openDrop.addEventListener("click", (e) => {
-    console.log(e.target.value);
+    const target = e.target.classList;
     if (openDrop.classList.contains("open")) {
         openDrop.classList.remove("open");
+        menu.classList.remove("active");
     } else {
         openDrop.classList.add("open");
+        menu.classList.add("active");
     }
+
+    openItem.addEventListener("click", () => {
+        openItem.classList.toggle("active");
+    });
 });
