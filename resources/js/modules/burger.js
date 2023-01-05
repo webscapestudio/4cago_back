803e621963f212af8d1074b939b82bdab44e8d28
Евгений Дианов <dianov.js@gmail.com>
2023-01-05 15:05:28 +0300

    Menu fix

--- resources/js/modules/burger.js
+++ resources/js/modules/burger.js
@@ -1,6 +1,16 @@
-const burgerBtn = document.querySelector('.burger')
-const menu = document.querySelector('.sidebar')
-burgerBtn.addEventListener('click', () => {
-  burgerBtn.classList.toggle('active')
-  menu.classList.toggle('active')
-})
+const burgerBtn = document.querySelector(".burger");
+const menu = document.querySelector(".sidebar");
+
+burgerBtn.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (burgerBtn.classList.contains("active")) {
+        burgerBtn.classList.remove("active");
+        menu.classList.remove("active");
+        document.body.classList.remove("overflow-hidden");
+    } else {
+        burgerBtn.classList.add("active");
+        menu.classList.add("active");
+        document.body.classList.add("overflow-hidden");
+    }
+    console.log(13123);
+});
