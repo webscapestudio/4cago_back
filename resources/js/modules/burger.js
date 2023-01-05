4d0516decf7d36d64c922388917e19b10a454d05
Евгений Дианов <dianov.js@gmail.com>
2023-01-05 15:14:11 +0300

    fix mob menu

--- resources/js/modules/burger.js
+++ resources/js/modules/burger.js
@@ -1,16 +1,17 @@
 const burgerBtn = document.querySelector(".burger");
 const menu = document.querySelector(".sidebar");
 
-burgerBtn.addEventListener("click", (e) => {
-    console.log(e.target);
-    if (burgerBtn.classList.contains("active")) {
-        burgerBtn.classList.remove("active");
-        menu.classList.remove("active");
-        document.body.classList.remove("overflow-hidden");
-    } else {
+window.addEventListener("click", (e) => {
+    const target = e.target.classList;
+    console.log(e.target.classList);
+    if (target.contains("burger") || target.contains("sidebar")) {
         burgerBtn.classList.add("active");
         menu.classList.add("active");
         document.body.classList.add("overflow-hidden");
+    } else {
+        burgerBtn.classList.remove("active");
+        menu.classList.remove("active");
+        document.body.classList.remove("overflow-hidden");
     }
     console.log(13123);
 });
