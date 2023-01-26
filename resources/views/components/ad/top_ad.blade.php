5e04736af121ef137ee21d8e59c598cb834a999b
Nikita5745 <sliplive1991@gmail.com>
2023-01-26 23:18:03 +1000

    111

--- resources/views/components/ad/top_ad.blade.php
+++ resources/views/components/ad/top_ad.blade.php
@@ -1,14 +1,14 @@
-  @if (!$upper_banner->count() == 0)
-    <div class="main-ad">
-      <a class="main-ad__link" href="{{ $upper_banner->link }}">
-        @if ($upper_banner->banner_image)
-          <picture>
-            <source srcset="{{ asset('storage/' . $upper_banner->banner_image) }}" type="image/webp"><img
-              src="{{ asset('storage/' . $upper_banner->banner_image) }}" alt="" />
-          </picture>
-        @else
-        @endif
-        <p class="ad-text">Реклама</p>
-      </a>
-    </div>
-  @endif
+ @if (!$upper_banner == null)
+   <div class="main-ad">
+     <a class="main-ad__link" href="{{ $upper_banner->link }}">
+       @if ($upper_banner->banner_image)
+         <picture>
+           <source srcset="{{ asset('storage/' . $upper_banner->banner_image) }}" type="image/webp"><img
+             src="{{ asset('storage/' . $upper_banner->banner_image) }}" alt="" />
+         </picture>
+       @else
+       @endif
+       <p class="ad-text">Реклама</p>
+     </a>
+   </div>
+ @endif
