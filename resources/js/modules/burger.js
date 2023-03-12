e75354524289f165b7f671fed4c0311328feefbf
Евгений Дианов <dianov.js@gmail.com>
2023-03-12 16:50:29 +0300

    FIX Aside mobile

--- resources/js/modules/burger.js
+++ resources/js/modules/burger.js
@@ -1,15 +1,18 @@
 const burgerBtn = document.querySelector(".burger");
 const menu = document.querySelector(".sidebar");
-
+const html = document.querySelector("html");
 burgerBtn.addEventListener("click", (e) => {
     const target = e.target.classList;
     if (target.contains("burger") && target.contains("active")) {
+        console.log(1);
         burgerBtn.classList.remove("active");
         menu.classList.remove("active");
-        document.querySelector("html").style.overflow = "";
+        document.documentElement.style.overflow = "";
     } else {
+        console.log(html);
+
         burgerBtn.classList.add("active");
         menu.classList.add("active");
-        document.querySelector("html").style.overflow = "hidden";
+        document.documentElement.style.overflow = "hidden";
     }
 });
