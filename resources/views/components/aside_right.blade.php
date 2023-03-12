9ee1b4963440c399ef12bd5416eec823fbf7aa53
Nikita5745 <sliplive1991@gmail.com>
2023-03-12 23:15:06 +1000

    фывф

--- resources/views/components/aside_right.blade.php
+++ resources/views/components/aside_right.blade.php
@@ -32,8 +32,7 @@
         <!-- item -->
         <div class="read__now-item">
 
-          <a class="title__link"
-            href="{{ route('post.show', [$post->category->slug, $post->slug]) }}">{{ $post->title }}</a>
+          <a class="title__link" href="">{{ $post->title }}</a>
           <div class="read__now-bottom">
             <div class="read__now-left">
               <div class="rn__left-item">
