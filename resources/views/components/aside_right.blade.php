31a3eef8bdc20479b31781518f3a754c8747341e
Nikita5745 <sliplive1991@gmail.com>
2023-03-12 22:15:34 +1000

    111

--- resources/views/components/aside_right.blade.php
+++ resources/views/components/aside_right.blade.php
@@ -31,7 +31,9 @@
       @foreach ($posts_read as $post)
         <!-- item -->
         <div class="read__now-item">
-
+          @php
+            var_dump($post->category->slug, $post->slug);
+          @endphp
           <a class="title__link"
             href="{{ route('post.show', [$post->category->slug, $post->slug]) }}">{{ $post->title }}</a>
           <div class="read__now-bottom">
